363fdb8f2be4dbef065611a9ac7faebae57a1650
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-11-25 15:16:40 +0400

    fix 25.11.2024

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -14,7 +14,7 @@ export let bodyUnlock = (delay = 500) => {
   if (bodyLockStatus) {
     setTimeout(() => {
       body.style.paddingRight = '0px';
-      document.querySelector('header').style.paddingRight = '0px';
+      document.querySelector('.header__inner').style.paddingRight = '0px';
       document.documentElement.classList.remove('lock');
     }, delay);
     bodyLockStatus = false;
@@ -29,7 +29,7 @@ export let bodyLock = (delay = 500) => {
     const getScrollbarWidth = () => window.innerWidth - document.documentElement.clientWidth;
     let scrollWith = getScrollbarWidth();
     body.style.paddingRight = `${scrollWith}px`;
-    document.querySelector('header').style.paddingRight = `${scrollWith}px`;
+    document.querySelector('.header__inner').style.paddingRight = `${scrollWith}px`;
     document.documentElement.classList.add('lock');
     bodyLockStatus = false;
     setTimeout(function () {
--- src/js/utils/form.js
+++ src/js/utils/form.js
@@ -29,17 +29,17 @@ const form = () => {
       //     parent.classList.add('_form-field');
       //   }
       // });
-      item.addEventListener('blur', () => {
+      item.addEventListener('input', () => {
         const inputValue = item.value.trim();
         const span = item.parentElement.nextElementSibling;
         const parent = item.parentElement.parentElement;
 
         if (!/^[a-zA-Zа-яА-Я\s\-]+$/.test(inputValue) || inputValue == '') {
-          span.classList.add('active');
+          // span.classList.add('active');
           parent.classList.add('_form-error');
                     parent.classList.remove('_form-field');
         } else {
-          span.classList.remove('active');
+          // span.classList.remove('active');
           parent.classList.remove('_form-error');
                    parent.classList.add('_form-field');
         }
@@ -54,7 +54,7 @@ const form = () => {
     const tel = document.querySelectorAll('.input--tel');
 
     tel.forEach((item) => {
-      item.addEventListener('blur', () => {
+      item.addEventListener('input', () => {
         const inputValue = item.value.trim();
         const span = item.parentElement.nextElementSibling;
         const parent = item.parentElement.parentElement;
@@ -78,17 +78,39 @@ const form = () => {
     const mail = document.querySelectorAll('.input--mail');
 
     mail.forEach((item) => {
-      item.addEventListener('blur', () => {
+      // item.addEventListener('blur', () => {
+      //   const inputValue = item.value.trim();
+      //   const span = item.parentElement.nextElementSibling;
+      //   const parent = item.parentElement.parentElement;
+
+      //   console.log(inputValue)
+
+      //   if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(inputValue) && inputValue != '') {
+      //     // span.classList.add('active');
+      //     parent.classList.add('_form-error');
+      //     parent.classList.remove('_form-field');
+      //   }
+      //   else if (inputValue === '') {
+      //     parent.classList.add('_form-error');
+      //     parent.classList.remove('_form-field');
+      //   } else {
+      //     // span.classList.remove('active');
+      //     parent.classList.remove('_form-error');
+      //          parent.classList.add('_form-field');
+      //   }
+      // });
+
+      item.addEventListener('input', () => {
         const inputValue = item.value.trim();
         const span = item.parentElement.nextElementSibling;
         const parent = item.parentElement.parentElement;
 
-        console.log(inputValue)
+        // console.log(inputValue)
 
         if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(inputValue) && inputValue != '') {
           // span.classList.add('active');
           parent.classList.add('_form-error');
-               parent.classList.remove('_form-field');
+          parent.classList.remove('_form-field');
         }
         else if (inputValue === '') {
           parent.classList.add('_form-error');
